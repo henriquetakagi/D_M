eb367f82b1f37f9766b96652869ce69ccdaa33a0
henriquetakagi <henrique.takagi@aluno.senai.br>
2026-05-27 21:40:52 -0400

    Terceiro Commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  databaseURL: "https://teste.firebaseio.com",
-  apiKey: "AIzaSyAEp-Xe0YFfLTk7EFApQG9RquVoG6fnpTc",
-  authDomain: "teste-36174.firebaseapp.com",
-  projectId: "teste-36174",
-  storageBucket: "teste-36174.firebasestorage.app",
-  messagingSenderId: "919017275837",
-  appId: "1:919017275837:web:ab601bdd47bc3fb74c12d4",
-  measurementId: "G-FNBK9DY66F"
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATA_BASE_URL,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
